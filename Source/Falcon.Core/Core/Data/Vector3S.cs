641a26f937e42d8c80fe982485ac66bb5b38c47f
Matthew Hawkeye Williamson <Hawkeye@76thvfw.org>
2021-02-10 10:17:54 -0600

    Implemented IEquatable<T> and IFormattable on Vector3S.

--- Source/Falcon.Core/Core/Data/Vector3S.cs
+++ Source/Falcon.Core/Core/Data/Vector3S.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 namespace Falcon.Core.Data
 {
     /// <summary>
     ///     Represents a 3-dimensional vector data structure using <see cref="short" />s as value types.
     /// </summary>
-    public struct Vector3S
+    public struct Vector3S : IEquatable<Vector3S>, IFormattable
     {
         #region Fields
 
         public static readonly Vector3S Zero = new Vector3S(0, 0, 0);
+
+        public static readonly Vector3S One = new Vector3S(1, 1, 1);
 
         public short X;
 
@@ -87,9 +91,9 @@ namespace Falcon.Core.Data
         {
             result = new Vector3S
             (
-                (short)(value.X > 0 ? value.X : -value.X),
-                (short)(value.Y > 0 ? value.Y : -value.Y),
-                (short)(value.Z > 0 ? value.Z : -value.Z)
+                (short) (value.X > 0 ? value.X : -value.X),
+                (short) (value.Y > 0 ? value.Y : -value.Y),
+                (short) (value.Z > 0 ? value.Z : -value.Z)
             );
         }
 
@@ -97,9 +101,9 @@ namespace Falcon.Core.Data
         {
             return new Vector3S
             (
-                (short)(value.X > 0 ? value.X : -value.X),
-                (short)(value.Y > 0 ? value.Y : -value.Y),
-                (short)(value.Z > 0 ? value.Z : -value.Z)
+                (short) (value.X > 0 ? value.X : -value.X),
+                (short) (value.Y > 0 ? value.Y : -value.Y),
+                (short) (value.Z > 0 ? value.Z : -value.Z)
             );
         }
 
@@ -167,30 +171,30 @@ namespace Falcon.Core.Data
 
         public static void DistanceSquared(ref Vector3S value1, ref Vector3S value2, out short result)
         {
-            short x = (short)(value1.X - value2.X);
-            short y = (short)(value1.Y - value2.Y);
+            short x = (short) (value1.X - value2.X);
+            short y = (short) (value1.Y - value2.Y);
             short z = (short) (value1.Z - value2.Z);
 
-            result = (short)((short)(x * x) + (short)(y * y) + (short)(z * z));
+            result = (short) ((short) (x * x) + (short) (y * y) + (short) (z * z));
         }
 
         public static short DistanceSquared(Vector3S value1, Vector3S value2)
         {
-            short x = (short)(value1.X - value2.X);
-            short y = (short)(value1.Y - value2.Y);
-            short z = (short)(value1.Z - value2.Z);
+            short x = (short) (value1.X - value2.X);
+            short y = (short) (value1.Y - value2.Y);
+            short z = (short) (value1.Z - value2.Z);
 
-            return (short)((short)(x * x) + (short)(y * y) + (short)(z * z));
+            return (short) ((short) (x * x) + (short) (y * y) + (short) (z * z));
         }
 
         public static void Dot(ref Vector3S left, ref Vector3S right, out short result)
         {
-            result = (short)((short) (left.X * right.X) + (short) (left.Y * right.Y) + (short) (left.Z * right.Z));
+            result = (short) ((short) (left.X * right.X) + (short) (left.Y * right.Y) + (short) (left.Z * right.Z));
         }
 
         public static short Dot(Vector3S left, Vector3S right)
         {
-            return (short)((short)(left.X * right.X) + (short)(left.Y * right.Y) + (short)(left.Z * right.Z));
+            return (short) ((short) (left.X * right.X) + (short) (left.Y * right.Y) + (short) (left.Z * right.Z));
         }
 
         public static void Normalize(ref Vector3S value, out Vector3S result)
@@ -208,7 +212,7 @@ namespace Falcon.Core.Data
 
         public short Length()
         {
-            return (short)Math.Sqrt(X * X + Y * Y + Z * Z);
+            return (short) Math.Sqrt(X * X + Y * Y + Z * Z);
         }
 
         public void Normalize()
@@ -217,7 +221,7 @@ namespace Falcon.Core.Data
 
             if (Math.Abs(length) < 1e-6) return; // All numbers smaller than 1e-6 are considered to be equal to zero.
 
-            short inv = (short)(1 / length);
+            short inv = (short) (1 / length);
             X *= inv;
             Y *= inv;
             Z *= inv;
@@ -260,10 +264,52 @@ namespace Falcon.Core.Data
 
         //    result = new Vector3S((short) ((1 + vectorValue.X) * 0.5 * width + x), (short) ((1 - vectorValue.Y) * 0.5 * height + y), (short) (vectorValue.Z * (maxZ - minZ) + minZ));
         //}
-
         public short [] ToArray()
         {
-            return new []{ X, Y, Z };
+            return new [] {X, Y, Z};
+        }
+
+        public string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            return format == null ? ToString(formatProvider) : string.Format(formatProvider, format, X.ToString(format, CultureInfo.CurrentCulture), Y.ToString(format, CultureInfo.CurrentCulture), Z.ToString(format, CultureInfo.CurrentCulture));
+        }
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.CurrentCulture, "X:{0} Y:{1} Z:{2}", X, Y, Z);
+        }
+
+        public string ToString(string format)
+        {
+            if (format == null)
+                return ToString();
+
+            return string.Format(CultureInfo.CurrentCulture, "X:{0} Y:{1} Z:{2}",
+                X.ToString(format, CultureInfo.CurrentCulture),
+                Y.ToString(format, CultureInfo.CurrentCulture), Z.ToString(format, CultureInfo.CurrentCulture));
+        }
+
+        public string ToString(IFormatProvider formatProvider)
+        {
+            return string.Format(formatProvider, "X:{0} Y:{1} Z:{2}", X, Y, Z);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return HashCode.Combine(X, Y, Z);
+            }
+        }
+
+        public bool Equals(Vector3S other)
+        {
+            return X == other.X && Y == other.Y && Z == other.Z;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Vector3S other && Equals(other);
         }
 
         #endregion
@@ -288,6 +334,18 @@ namespace Falcon.Core.Data
         public static Vector3S operator /(Vector3S value, short scale)
         {
             return new Vector3S((short) (value.X / scale), (short) (value.Y / scale), (short) (value.Z / scale));
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(Vector3S left, Vector3S right)
+        {
+            return left.Equals(right);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(Vector3S left, Vector3S right)
+        {
+            return !left.Equals(right);
         }
 
         #endregion
