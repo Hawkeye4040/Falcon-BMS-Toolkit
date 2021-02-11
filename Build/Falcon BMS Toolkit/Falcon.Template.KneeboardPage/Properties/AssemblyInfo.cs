3581de3a7f1d134beda32f45903380de69ebbff2
Matthew Hawkeye Williamson <Hawkeye@76thvfw.org>
2021-02-11 09:52:06 -0600

    Updated VSIX projects assembly info.

--- Build/Falcon BMS Toolkit/Falcon.Template.KneeboardPage/Properties/AssemblyInfo.cs
+++ Build/Falcon BMS Toolkit/Falcon.Template.KneeboardPage/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Falcon.Template.KneeboardPage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Kneeboard Page Template for Falcon Planner.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Matthew Williamson")]
+[assembly: AssemblyCompany("Matthew 'Hawkeye' Williamson")]
 [assembly: AssemblyProduct("Falcon.Template.KneeboardPage")]
-[assembly: AssemblyCopyright("Copyright © Matthew Williamson 2021")]
+[assembly: AssemblyCopyright("Copyright © 2021 Matthew 'Hawkeye' Williamson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
--- Source/Falcon.VSIX/Properties/AssemblyInfo.cs
+++ Source/Falcon.VSIX/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Falcon.VSIX")]
 [assembly: AssemblyDescription("Visual Studio Extension for Falcon BMS Toolkit.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Matthew 'Hawkeye' Williamson")]
 [assembly: AssemblyProduct("Falcon.VSIX")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © 2021 Matthew 'Hawkeye' Williamson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,3 +32,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
