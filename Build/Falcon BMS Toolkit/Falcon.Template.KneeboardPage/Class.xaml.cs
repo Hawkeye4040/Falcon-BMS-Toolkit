59d3b3fed58412395b78dcf5f84822f578ce6304
Matthew Hawkeye Williamson <Hawkeye@76thvfw.org>
2021-02-11 08:39:50 -0600

    Added Kneeboard Page file template.

---
+++ Build/Falcon BMS Toolkit/Falcon.Template.KneeboardPage/Class.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+    
+$
+if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace $rootnamespace$
+{
+    public partial class $safeitemrootname$
+    {
+        public $safeitemrootname$()
+        {
+            InitializeComponent();
+        }
+    }
+}
