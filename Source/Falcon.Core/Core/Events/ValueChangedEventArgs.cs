0610b92c0b3bc0cd58da2db8a2e4124d8908c427
Matthew 'Hawkeye' Williamson <Hawkeye@76thvfw.org>
2021-03-26 11:06:53 -0500

    Added generic values changed event argument classes.

---
+++ Source/Falcon.Core/Core/Events/ValueChangedEventArgs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Falcon.Core.Events
+{
+    public sealed class ValueChangedEventArgs<T> : EventArgs
+    {
+        #region Properties
+
+        public T OldValue { get; }
+
+        public T NewValue { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public ValueChangedEventArgs(T oldValue, T newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
+        #endregion
+    }
+
+    public sealed class ValuesChangedEventArgs<T> : EventArgs
+    {
+        #region Properties
+
+        public List<T> OldValues { get; }
+
+        public List<T> NewValues { get; }
+
+        public List<T> AddedValues => NewValues.Where(value => !OldValues.Contains(value)).ToList();
+
+        public List<T> RemovedValues => OldValues.Where(value => !NewValues.Contains(value)).ToList();
+
+        #endregion
+
+        #region Constructors
+
+        public ValuesChangedEventArgs(IEnumerable<T> oldValues, IEnumerable<T> newValues)
+        {
+            OldValues = new List<T>();
+            OldValues.AddRange(oldValues);
+            NewValues = new List<T>();
+            NewValues.AddRange(newValues);
+        }
+
+        #endregion
+    }
+
+    public delegate void OnValueChanged<T>(object sender, ValueChangedEventArgs<T> e);
+
+    public delegate void OnValuesChanged<T>(object sender, ValuesChangedEventArgs<T> e);
+}
