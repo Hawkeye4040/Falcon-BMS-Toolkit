c754843a9965a204395243252ade039c69e4ec86
Matthew 'Hawkeye' Williamson <Hawkeye@76thvfw.org>
2021-04-19 10:21:48 -0500

    Added Mission Plan model class.

---
+++ Source/Falcon.Core/Core/MissionPlan.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Falcon.Core
+{
+    public sealed class MissionPlan
+    {
+        #region Methods
+
+        public static async Task<bool> Load(string filePath)
+        {
+            Uri uri = new Uri(filePath, UriKind.Absolute);
+
+            bool successful = await Load(uri);
+
+            return successful;
+        }
+
+        public static async Task<bool> Load(Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Save(string filePath)
+        {
+            Uri uri = new Uri(filePath, UriKind.Absolute);
+
+            bool successful = await Save(uri);
+
+            return successful;
+        }
+
+        public async Task<bool> Save(Uri filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
