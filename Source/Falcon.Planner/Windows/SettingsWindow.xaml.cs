ccf6347f2e066c893b872f534038ed351d079c3e
Matthew 'Hawkeye' Williamson <Hawkeye@76thvfw.org>
2021-04-19 10:29:19 -0500

    Added content to settings window

---
+++ Source/Falcon.Planner/Windows/SettingsWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace Falcon.Planner.Windows
+{
+    /// <summary>
+    ///     Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public sealed partial class SettingsWindow
+    {
+        #region Constructors
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void InstallationDirectoryBrowserButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
